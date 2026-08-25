769d572fc6ce651cefcf348e1c597cc8922a6976
M-chlihi <chlihi101mohamed@gmail.com>
2026-08-25 21:15:35 +0100

    refactor : roles middlware reffactor security

--- backend/middleware/verifyRoles.js
+++ backend/middleware/verifyRoles.js
@@ -1,5 +1,9 @@
 const verifyRoles = (...allowedRoles) => {
   return (req, res, next) => {
+    console.log("REQUEST:", req.method, req.originalUrl);
+
+    console.log("JWT roles:", req.roles);
+    console.log("Allowed roles:", allowedRoles);
     if (!req?.roles) {
       return res.sendStatus(401);
     }
